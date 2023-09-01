2a2430b462c05fc2a2853753c3f28c7aa054f372
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-09-01 12:28:57 +0700

    Move to Video Quality Menu

--- app/src/main/java/app/revanced/integrations/patches/ads/VideoQualityMenuFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/VideoQualityMenuFilter.java
@@ -9,15 +9,29 @@ public final class VideoQualityMenuFilter extends Filter {
     public static volatile boolean isVideoQualityMenuVisible;
 
     public VideoQualityMenuFilter() {
-        pathFilterGroups.addAll(new StringFilterGroup(
+        final var oldqualityflyout = new StringFilterGroup(
                 SettingsEnum.ENABLE_OLD_QUALITY_LAYOUT,
                 "quick_quality_sheet_content.eml-js"
         ));
+
+        final var qualityFooter = new StringFilterGroup(
+                SettingsEnum.HIDE_PLAYER_FLYOUT_PANEL_QUALITY_FOOTER,
+                "quality_sheet_footer",
+                "divider"
+        );
+
+        this.pathFilterGroups.addAll(
+                oldqualityflyout,
+                qualityFooter
+        );
     }
 
     @Override
     boolean isFiltered(String path, @Nullable String identifier, String allValue, byte[] protobufBufferArray,
                        FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
+        if (!path.contains("quality_sheet_content.eml-js")) {
+            return false;
+        }
         isVideoQualityMenuVisible = true;
 
         return false;
