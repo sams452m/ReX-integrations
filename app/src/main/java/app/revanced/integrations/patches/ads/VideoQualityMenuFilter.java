88431efbce0d0a591af74837f35cb53889a08685
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-09-01 13:05:57 +0700

    Revert

--- app/src/main/java/app/revanced/integrations/patches/ads/VideoQualityMenuFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/VideoQualityMenuFilter.java
@@ -9,31 +9,17 @@ public final class VideoQualityMenuFilter extends Filter {
     public static volatile boolean isVideoQualityMenuVisible;
 
     public VideoQualityMenuFilter() {
-        final var oldqualityflyout = new StringFilterGroup(
+        pathFilterGroups.addAll(new StringFilterGroup(
                 SettingsEnum.ENABLE_OLD_QUALITY_LAYOUT,
                 "quick_quality_sheet_content.eml-js"
-        );
-
-        final var qualityFooter = new StringFilterGroup(
-                SettingsEnum.HIDE_PLAYER_FLYOUT_PANEL_QUALITY_FOOTER,
-                "quality_sheet_footer",
-                "divider"
-        );
-
-        this.pathFilterGroups.addAll(
-                oldqualityflyout,
-                qualityFooter
-        );
+        ));
     }
 
     @Override
     boolean isFiltered(String path, @Nullable String identifier, String allValue, byte[] protobufBufferArray,
                        FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
-        if (!path.contains("quality_sheet_content.eml-js")) {
-            return false;
-        }
         isVideoQualityMenuVisible = true;
 
-        return super.isFiltered(path, identifier, allValue, protobufBufferArray, matchedList, matchedGroup, matchedIndex);
+        return false;
     }
 }
